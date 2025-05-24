e15302a99a4676b36e99c28aa16b12e3e9f98856
Azizul Kabir Jayed <121378902+azizulkabirjayed@users.noreply.github.com>
2025-05-24 23:03:19 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('year').innerText = (new Date()).getFullYear();
+document.getElementById('last_modified').innerText = document.lastModified;;
+document.getElementById('page_location').innerText = window.location.href;
